cae8db97a15b7acd879820c0f1f5903f907e6130
Julia Pickel <juliapickel@gmx.de>
2023-11-15 18:24:51 +0100

    completed US1

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,80 @@
+import { outivities } from "@/lib/data";
+import Image from "next/image";
+import styled from "styled-components";
+
 export default function HomePage() {
-  return (
-    <div>
-      <h1>Hello from Next.js</h1>
-    </div>
-  );
+  try {
+    return (
+      <>
+        <StyledTitle>All Outivities</StyledTitle>
+        <StyledOutivitieCardContainer>
+          {outivities.map((outivity) => (
+            <StyledOutivitieCard key={outivity.id}>
+              <StyledImage
+                src={outivity.image}
+                alt={outivity.title}
+                width={300}
+                height={200}
+              />
+              <StyledOutivityTitle>{outivity.title}</StyledOutivityTitle>
+              <StyledOutivityCityName>
+                in {outivity.location}
+              </StyledOutivityCityName>
+            </StyledOutivitieCard>
+          ))}
+        </StyledOutivitieCardContainer>
+      </>
+    );
+  } catch (error) {
+    console.log(error);
+    return (
+      <>
+        <h1>Ooops... </h1>
+        <p>
+          Error Loading Outivities. We were unable to load the Outivities list.
+          Please try again later.
+        </p>
+      </>
+    );
+  }
 }
+
+const StyledTitle = styled.h1`
+  display: grid;
+  height: 2em;
+  place-content: center;
+  background-color: var(--primary-color);
+  color: var(--neutral-color);
+`;
+
+const StyledOutivitieCardContainer = styled.ul`
+  display: grid;
+  gap: 1rem;
+  padding: 20px;
+  place-content: center;
+  gap: 50px;
+`;
+
+const StyledOutivitieCard = styled.li`
+  position: relative;
+  place-content: center;
+  max-width: 640px;
+  border-radius: 5px;
+  overflow: hidden;
+  box-shadow: var(--neutral-color) 0px 2px 1px, var(--neutral-color) 0px 4px 2px,
+    var(--neutral-color) 0px 8px 4px, var(--neutral-color) 0px 16px 8px,
+    var(--neutral-color) 0px 32px 16px;
+`;
+
+const StyledImage = styled(Image)`
+  overflow: clip;
+  overflow-clip-margin: content-box;
+`;
+
+const StyledOutivityTitle = styled.h2`
+  margin: 10px 20px 0px 20px;
+`;
+
+const StyledOutivityCityName = styled.p`
+  margin: 0px 20px 20px 20px;
+`;
